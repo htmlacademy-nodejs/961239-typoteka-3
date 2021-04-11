48147b38f7c45aa332afc5af4a99365dd01872f7
Aleksei Litvinenko <alexlit.double@gmail.com>
2021-04-11 23:03:20 +0700

    Шаблонизация (#9)

--- src/express/express.js
+++ src/express/express.js
@@ -1,17 +1,34 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const {URL} = require(`./../service/constants`);
 const baseRouter = require(`./routes/baseRouter`);
 const myRouter = require(`./routes/myRouter`);
 const articlesRouter = require(`./routes/articlesRouter`);
+const PUBLIC_PATH = path.resolve(__dirname, `public`);
+const TEMPLATE_PATH = path.resolve(__dirname, `templates`);
 
 const PORT = 8080;
 
 const app = express();
 
+
+app.use(express.static(PUBLIC_PATH));
+app.use(URL.ARTICLES, express.static(PUBLIC_PATH));
+app.set(`views`, TEMPLATE_PATH);
+app.set(`view engine`, `pug`);
 app.use(URL.ARTICLES, articlesRouter);
 app.use(URL.BASE, baseRouter);
 app.use(URL.MY, myRouter);
+
+app.use((req, res) => {
+  res.status(404).render(`404`);
+});
+
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).render(`500`);
+});
 
 app.listen(PORT);
