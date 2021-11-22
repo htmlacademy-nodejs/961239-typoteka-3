b4b5acf8e7b2f6ba6362f86451e5c75bad3c79e3
litvinenkoAlexlit <alexlit.double@gmail.com>
2021-11-22 23:28:12 +0700

    Финальные правки

--- src/utils/dev-utils.js
+++ src/utils/dev-utils.js
@@ -3,10 +3,10 @@
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const shuffle = (someArray) => {
-  for (let i = someArray.length - 1; i > 0; i--) {
+  someArray.forEach((elem, i) => {
     const randomPosition = Math.floor(Math.random() * i);
     [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
-  }
+  });
 
   return someArray;
 };
